7445914d42906475ec003a1bf1ff4e1f38b2e193
LvInSaNevL <mattwollam@gmail.com>
2019-12-05 16:59:53 -0500

    started working on the rust build

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+mod ui_manager;
+mod io_manager;
+use gtk::*;
+use std::process;
+use std::env;
+
+// Init point for LiLio
 fn main() {
-    println!("Hello, world!");
+    io_manager::splashPrint();
+    // Checks the arguments passed at the command line
+    io_manager::flagParse(env::args().collect());
+
+    // Inits GTK before rest of the codebase
+    if gtk::init().is_err() {
+        eprintln!("Failed to initialize GTK Application");
+        process::exit(1);
+    }
+
+    // Inits the UI and widgets
+    let app = ui_manager::App::new();
+    app.window.show_all();
+
+    // Enter the GTK main event loop
+    gtk::main();
 }
---
+++ src/ui_manager.rs
@@ -0,0 +1,49 @@
+extern crate gtk;
+use gtk::*;
+
+pub struct App {
+    pub window: Window,
+    pub header: Header,
+}
+
+pub struct Header {
+    pub container: HeaderBar
+}
+
+// Creating the actual root window for LiLio
+impl App {
+    pub fn new() -> App {
+        // Defining the root window the header bar
+        let window = Window::new(WindowType::Toplevel);
+        let header = Header::new();
+
+        // Setting the window name for the user and wm
+        window.set_titlebar(&header.container);
+        window.set_title("LiLio");
+        window.set_wmclass("lilio", "LiLio");
+
+        // Exit button logic
+        window.connect_delete_event(move |_, _| {
+            main_quit();
+            Inhibit(false)
+        });
+
+        // Return to the main program
+        App { window, header }
+    }
+}
+
+// Creates a proper header with window controls
+impl Header {
+    fn new() -> Header {
+        // Creates the header container
+        let container = HeaderBar::new();
+
+        // Sets the header text
+        container.set_title("LiLio");
+        container.set_show_close_button(true);
+
+        // Jumps back to main logic
+        Header { container }
+    }
+}
